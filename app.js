156b0472f8186bc9432ec19d54e6bbec6298675d
John Hawley <jhawl42@gmail.com>
2020-03-29 21:40:35 -0500

    first release 0.1

--- app.js
+++ app.js
@@ -1,51 +1,84 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var app = express();
-var http = require("http").createServer(app);
-var io = require("socket.io")(http);
+const express = require('express');
+const fs = require('fs-extra');
+const path = require('path');
+const bodyParser = require('body-parser');
+const app = express();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
 const portNum = 8080;
-var port = process.env.PORT || portNum;
-//http.listen(port);
+const port = process.env.PORT || portNum;
 
-var fs = require('fs');
-var path = require('path');
-var dataFile = path.join(__dirname + '/assets/data.json');
-var cacheFile = path.join(__dirname + '/assets/cache.json');
-var connectedSockets = [];
 
+// Storage
+const datapath = process.cwd() + '/lilwont-dash-data/';
+const dataFile = path.join(datapath + 'data.json');
+// const cacheFile = path.join(datapath + 'cache.json');
+
+//install check
+const defaultVals = {
+  "lilwont": {
+    "goal": "100",
+    "bank": "87",
+    "buy": "10",
+    "cash": "1132"
+  }
+}
+
+// Create data files
+async function createDataFiles(f) {
+  try {
+    await fs.ensureFile(f + 'data.json')
+    console.log("created file");
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+createDataFiles(datapath);
+
+
+// Begin app.js
+let connectedSockets = [];
 app.use(bodyParser.json());
 
-// app.use(express.static(__dirname + '/'));
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/views/dashboard.html'));
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname + '/views/dashboard.html'));
 });
-app.get('/view/:item', function(req, res) {
-    res.sendFile(path.join(__dirname + '/views/' + req.params.item + '.html'));
+app.get('/view/:item', function (req, res) {
+  res.sendFile(path.join(__dirname + '/views/' + req.params.item + '.html'));
 });
-app.get('*/css/:file', function(req, res) {
-    res.sendFile(path.join(__dirname + '/assets/css/' + req.params.file));
+app.get('*/css/:file', function (req, res) {
+  res.sendFile(path.join(__dirname + '/assets/css/' + req.params.file));
 });
-app.get('*/js/:file', function(req, res) {
-    res.sendFile(path.join(__dirname + '/assets/js/' + req.params.file));
+app.get('*/js/:file', function (req, res) {
+  res.sendFile(path.join(__dirname + '/assets/js/' + req.params.file));
 });
-app.get('/dashboard', function(req, res) {
-    res.sendFile(path.join(__dirname + '/views/dashboard.html'));
+app.get('/dashboard', function (req, res) {
+  res.sendFile(path.join(__dirname + '/views/dashboard.html'));
 });
-app.get('/assets/:asset', function(req, res) {
-    res.sendFile(path.join(__dirname + '/assets/' + req.params.asset));
+app.get('/assets/:asset', function (req, res) {
+  res.sendFile(path.join(__dirname + '/assets/' + req.params.asset));
 });
-app.post('/endpoint', function(req, res) {
-    var file = require(dataFile);
-    file.lilwont.buy = req.body.dayIn;
-    file.lilwont.cash = req.body.dayOut;
-    file.lilwont.bank = req.body.bnkNow;
-    file.lilwont.goal = req.body.bnkGoal;
-    fs.writeFile(dataFile, JSON.stringify(file, null, 2), function(err) {
-        if (err)
-            return console.log(err);
-    });
-    res.send(req.body);
-    // res.send('yeah');
+app.get('/data', function (req, res) {
+  res.sendFile(dataFile);
+});
+app.post('/endpoint', function (req, res) {
+  var file = fs.readFileSync(dataFile);
+  if (file) {
+    file = JSON.parse(file);
+  } else {
+    file = {};
+    file.lilwont = {};
+  }
+  file.lilwont.buy = req.body.dayIn;
+  file.lilwont.cash = req.body.dayOut;
+  file.lilwont.bank = req.body.bnkNow;
+  file.lilwont.goal = req.body.bnkGoal;
+  fs.writeFile(dataFile, JSON.stringify(file, null, 2), function (err) {
+    if (err)
+      return console.log(err);
+  });
+  res.send(req.body);
 });
 // app.get('/socket.io/:file', function(req, res) {
 //     console.log('Load: ' + req.params.file);
@@ -54,24 +87,31 @@ app.post('/endpoint', function(req, res) {
 
 // var io = require('socket.io').listen(app);
 
-io.sockets.on('connection', function(socket) {
-    connectedSockets.push(socket);
+io.sockets.on('connection', function (socket) {
+  connectedSockets.push(socket);
 });
 
-// Start watching our cache file
-fs.watch(dataFile, function(event, filename) {
+setTimeout(() => {
+  fs.writeFile(dataFile, JSON.stringify(defaultVals, null, 2), function (err) {
+    if (err)
+      return console.log(err);
+  });
+
+  // Start watching our cache file
+  fs.watch(dataFile, function (event, filename) {
     if (event == 'change') {
-        fs.readFile(dataFile, function(err, data) {
-            if (err)
-                throw err;
-            connectedSockets.forEach(function(socket) {
-                socket.emit('data', JSON.parse(data));
-            });
+      fs.readFile(dataFile, function (err, data) {
+        if (err)
+          throw err;
+        connectedSockets.forEach(function (socket) {
+          socket.emit('data', JSON.parse(data));
         });
+      });
     }
-});
+  });
+}, 2000);
 
-http.listen(port, function() {
-    //server started
-    console.log("\nService Started\nWelcome lilwont!\n\nDashboard is at:\nhttp://localhost:"+portNum+"/dashboard\n\nCtrl+C to exit.");
+http.listen(port, function () {
+  //server started
+  console.log("\nService Started\nWelcome lilwont!\n\nDashboard is at:\nhttp://localhost:" + portNum + "/dashboard\n\nCtrl+C to exit.");
 });
--- readme.md
+++ readme.md
@@ -2,10 +2,15 @@
 
 This is a dashboard web app created for the twitch channel lilwont.
 
-##Install
+## NOTICE!
 
-                
-1. Item A
-2. Item B
-3. Item C
+> :warning: **SLOPPY CODE**: This is awful code that cotains several bad practices and assumptions, please don't view this as a reference to my ability. I am aware of all the awful attrocities that I have done here, thanks.
+
+
+### Install
+
+1. Downlaod release
+2. Execute .exe
+3. A `lilwont-dash-data` directory will be created somewhere on your machine, it will house persistent local data storage.
+4. Only one instance can run at a time.
                 
